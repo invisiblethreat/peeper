ca59100bb448092d3ac8ce35657b46fa7ba1f59d
Scott Walsh <scott@invisiblethreat.ca>
2013-04-04 23:41:26 -0300

    Fixed logging

--- capture.js
+++ capture.js
@@ -9,13 +9,13 @@ page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
 page.open(url, function (status) {
   if ( status !== 'success')
   {
-    console.log('FAILED to load the address ' + url );
+    console.log("FAILED to load the address",  url);
     phantom.exit();
   }
   window.setTimeout(function ()
   {
     page.render(filename);
-//    console.log('Rendered ' + filename ' from ' + url );
+    console.log("Rendered", filename, "from", url);
     phantom.exit();
   }, 2000);
 });
